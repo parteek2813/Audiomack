2670250ed5e55ad53575771c423be647b843a5f2
parteek kumar <digitalparteek@gmail.com>
2023-10-27 19:52:04 +0530

    otp verification using specific number complete

--- src/screens/otpVerify/OtpVerify.jsx
+++ src/screens/otpVerify/OtpVerify.jsx
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import otp_box from "../../assets/otp_box.svg";
 import "./Style.css";
 import { Link } from "react-router-dom";
 
 function OtpVerify() {
+  const [otpValues, setOtpValues] = useState(["", "", "", ""]); // State to store OTP input values
+  const [isOtpCorrect, setIsOtpCorrect] = useState(false); // State to track if OTP is correct
+
+  const handleOtpChange = (index, value) => {
+    let newOtpValues = [...otpValues];
+    newOtpValues[index] = value;
+    setOtpValues(newOtpValues);
+    setIsOtpCorrect(false); // Reset the state when OTP values change
+  };
+
+  const handleSubmit = () => {
+    const enteredOtp = otpValues.join(""); // Concatenate OTP input values into a single string
+    if (enteredOtp === "5678") {
+      setIsOtpCorrect(true); // Set state to indicate correct OTP
+    } else {
+      setIsOtpCorrect(false); // Set state to indicate incorrect OTP
+    }
+  };
+
   return (
     <>
       <div className="container">
         <div className="group-wrapper">
           <div className="group">
             <button className="btn">
-              <Link to="/player">
-                <div className="overlap-group">
+              {/* {otpValues.join("") === "5678" && ( */}
+              <Link to={isOtpCorrect ? "/player" : "#"}>
+                <button className="overlap-group" onClick={handleSubmit}>
                   <div className="text-wrapper">Verify</div>
-                </div>
+                </button>
               </Link>
+              {/* )} */}
 
               <p className="resend_p">Resend OTP</p>
               <p className="another_p">Use another number</p>
@@ -26,31 +47,16 @@ function OtpVerify() {
             </p>
 
             <div className="otp_input_box">
-              <input
-                className="input_box_css"
-                type="text"
-                src={otp_box}
-                maxLength={1}
-                autoFocus
-              ></input>
-              <input
-                className="input_box_css"
-                type="text"
-                src={otp_box}
-                maxLength={1}
-              ></input>
-              <input
-                className="input_box_css"
-                type="text"
-                src={otp_box}
-                maxLength={1}
-              ></input>
-              <input
-                className="input_box_css"
-                type="text"
-                src={otp_box}
-                maxLength={1}
-              ></input>
+              {otpValues.map((value, index) => (
+                <input
+                  key={index}
+                  className="input_box_css"
+                  type="text"
+                  maxLength={1}
+                  value={value}
+                  onChange={(e) => handleOtpChange(index, e.target.value)}
+                />
+              ))}
             </div>
 
             <button className="text-wrapper-2">OTP Verification</button>
