8017da30d46430c81f88f32e69e7d1b0754f6826
parteek kumar <digitalparteek@gmail.com>
2023-10-27 16:56:11 +0530

    Added center elements

--- src/screens/player/PlayerScreen.jsx
+++ src/screens/player/PlayerScreen.jsx
@@ -3,6 +3,7 @@ import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 import "./Style.css";
 import SidebarMenuIcon from "../../assets/sidebar_menu_icon.svg";
 import BottomMenuIcon from "../../assets/Bottom_menu_icon.svg";
+import StrokeVector from "../../assets/stroke_vector.svg";
 
 function PlayerScreen() {
   return (
@@ -23,24 +24,34 @@ function PlayerScreen() {
           </div>
         </div>
 
-        <div className="centerMainContainer">
-          {/* Upper Center Container */}
-          <div className="centerContainer">
-            <p className="first_text">First-level Menu</p>
-            <p className="first_slash">/</p>
-            <p className="second_text">Second-level Menu</p>
-            <p className="second_slash">/</p>
-            <p className="third_text">Current Page</p>
+        <div className="flex flex-col">
+          <div className="centerMainContainer">
+            {/* Upper Center Container */}
+            <div className="centerContainer">
+              <p className="first_text">First-level Menu</p>
+              <p className="first_slash">/</p>
+              <p className="second_text">Second-level Menu</p>
+              <p className="second_slash">/</p>
+              <p className="third_text">Current Page</p>
+            </div>
+
+            {/* Lower Upper Center Container */}
+
+            <div className="LowerLeftCenterContainer">
+              <p className="LowerLeftText">Songs</p>
+              <button className="LowerLeftButton">
+                <p className="textAddSongs">Add Songs</p>
+              </button>
+            </div>
+            <img className="stroke_vector_svg" src={StrokeVector} />
           </div>
 
-          {/* Lower Upper Center Container */}
-
-          <div className="LowerLeftCenterContainer">
-            <p className="LowerLeftText">Songs</p>
-            <button className="LowerLeftButton">
-              <p className="textAddSongs">Add Songs</p>
-            </button>
+          <div className="flex flex-row">
+            <p className="songNameText">SONG NAME</p>
+            <p className="sourceNameText">SOURCE</p>
+            <p className="addedOnNameText">ADDED ON</p>
           </div>
+          <img className="stroke_vector_svg mt-[20px]" src={StrokeVector} />
         </div>
       </div>
     </>
